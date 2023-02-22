64ed2bd0f2ad43f431b6b731fc3847c571db197e
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-02-22 10:38:20 +0000

    [Add] topページ作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,7 @@ class BooksController < ApplicationController
   
   def index
     @user = User.find(current_user.id)
-    
     @post_book = Book.new
-    
     @books = Book.all
   end
   
@@ -23,9 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    
     @user = User.find(@book.user_id)
-    
     @post_book = Book.new
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,14 @@ class UsersController < ApplicationController
   
   def index
     @user = User.find(current_user.id)
-    
     @users = User.all
-    
     @post_book = Book.new
   end
   
   def show
     @user = User.find(params[:id])
-    
-    
     @post_book = Book.new
-    
-    
     @books = @user.books
-    
   end
   
   def edit
