ee0e9eec4cf0c4d9411c96adfbb59fff6f610cce
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-02-23 09:28:31 +0000

    [Fix] 余計な空白の削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :books, only: [:index, :show, :edit, :create, :update, :destroy ]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
   
 end
